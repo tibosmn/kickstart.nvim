f3293d2791bf267c1d5702b6b53a1e4088b89406
tibosmn <contact@thibaultsimon.fr>
2025-05-08 19:22:05 +0200

    chore(linters): specified linters

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,41 +5,21 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
-
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      --lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft['python'] = { 'flake8' }
+      lint.linters_by_ft['lua'] = { 'luacheck' }
+      lint.linters_by_ft['typescript'] = { 'eslint_d' }
+      lint.linters_by_ft['javascript'] = { 'eslint_d' }
+
+      -- Override flake8 config to disable E501 (line too long), is handled by formatting with black
+      lint.linters.flake8 = {
+        cmd = 'flake8',
+        stdin = true,
+        args = { '--ignore=E501', '-' },
+      }
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
