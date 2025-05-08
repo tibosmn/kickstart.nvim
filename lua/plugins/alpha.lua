9981773fde945023664b91ac87c37e7ef761154f
tibosmn <contact@thibaultsimon.fr>
2025-05-08 19:20:40 +0200

    feat(dashboard): alpha as a welcome screen

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,45 @@
+-- Sashboard
+return {
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'echasnovski/mini.icons',
+  },
+
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+    -- Set header
+    dashboard.section.header.val = {
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('p', '  > Find project', function()
+        require('telescope').extensions.project.project()
+      end),
+      dashboard.button('f', '  > Find file', ':cd $HOME/Documents/projects | Telescope find_files<CR>'),
+      dashboard.button('t', '  > Find text', ': cd $HOME/Documents/projects | Telescope live_grep <CR>'),
+      dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
+      dashboard.button('s', '  > Settings', ':e $MYVIMRC | :cd %:p:h | pwd<CR>'),
+      dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+    }
+
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd [[
+      autocmd FileType alpha setlocal nofoldenable
+    ]]
+  end,
+}
