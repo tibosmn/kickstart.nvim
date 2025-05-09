54fae1aed0928d0de022f3a47ff0ae74ca583b68
tibosmn <contact@thibaultsimon.fr>
2025-05-09 15:00:35 +0200

    feat(plugins): added commentary and vim-tmux

---
+++ lua/plugins/commentary.lua
@@ -0,0 +1,6 @@
+-- Comment multiple lines at once
+
+return {
+  'tpope/vim-commentary',
+  event = 'VeryLazy',
+}
---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,20 @@
+-- Navigate seamlessly between vim and tmux splits using a consistent set of hotkeys.
+
+return {
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+    'TmuxNavigatorProcessList',
+  },
+  keys = {
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+  },
+}
